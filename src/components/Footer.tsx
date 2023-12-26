e8b3c2d4d8a71f5d5c29ef00b3bce7995f65543b
Anil Banjade <anil.banjade17@gmail.com>
2023-12-26 19:17:27 +0545

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,34 +1,78 @@
-import { _SocialLinks } from "../assets/data/Yathartha"
-
+import React from "react";
+import Logo from "../assets/logo.jpg";
+import "../styles/footer.css"
+import {
+  FaLinkedin,
+  FaInstagram,
+  FaFacebook,
+  FaEnvelope,
+} from "react-icons/fa";
+import { FaMobileScreen, FaRegEnvelope, FaLocationDot } from "react-icons/fa6";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
   return (
-    <footer className="flex flex-col justify-evenly items-center w-[100%] min-h-[20rem] bg-theme-alt">
-        <Socials />
-        <Copyright />
-    </footer>
-  )
-}
+    <div className="footer">
+      <div className="top-footer">
+        <div className="motto">
+            <img src={Logo} alt="Logo" className="footer-logo"/>
+            <h1>YATHARTHA</h1>
+          
+          <h2>Connecting Engineers</h2>
+        </div>
+        <div className="quick-links">
+          <h1>Quick Links</h1>
+          <ul className="links">
+            <li>Home</li>
+            <li>Events</li>
+            <li>Projects</li>
+            <li>About Us</li>
+            <li>Contact Us</li>
+          </ul>
+        </div>
+        <div className="contact-us">
+          <h1>Contact Us</h1>
 
-const Socials : React.FC = () =>
-{
-    return (
-        <ul className="w-[50%] h-[10rem] flex flex-col justify-evenly items-center text-white">
-            {
-                _SocialLinks.map(({media, url})=>
-                {
-                    return <li><a href={url}  className="p-2 m-2 rounded-sm hover:underline hover:underline-offset-2 transition-all duration-300">{media}</a></li>
-                })
-            }
-        </ul>
-    )
-}
+          <div className="mobile">
+            <FaMobileScreen />
+            <span>+977 984534343, +977 988786876</span>
+          </div>
 
-const Copyright : React.FC = () =>
-{
-    return (
-        <p className="text-theme">All Copyright reserved © 2024</p>
-    )
-}
+          <div className="email">
+            <FaRegEnvelope />
+            <span>techfest@tcioe.edu.np</span>
+          </div>
 
-export default Footer
+          <div className="location">
+            <FaLocationDot />
+            <span>IOE Thapathali Campus, Kathmandu, Nepal</span>
+          </div>
+        </div>
+      </div>
+      <div className="social">
+        <div className="social-title">
+        <h2>Connect with us on:</h2>
+        </div>
+        <div className="social-links">
+          <div className="instagram">
+            <FaInstagram />
+          </div>
+          <div className="linkedin">
+            <FaLinkedin />
+          </div>
+          <div className="facebook">
+            <FaFacebook />
+          </div>
+          <div className="mail">
+            <FaEnvelope />
+          </div>
+        </div>
+        <div className="copyright">
+          Copyright &copy; {currentYear} | Yathartha | All rights reserved.
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
