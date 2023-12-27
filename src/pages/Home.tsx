d4cb5aa9dc11f8a1eaf3ae11b359dd5fa5d49ad2
Roshan Thapa <thaparoshan3722@gmail.com>
2023-12-27 19:05:10 +0545

    add: hero image added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import {  _EventDetails } from "../assets/data/Yathartha.ts" ;
 import CountDown from "../components/CountDown.tsx";
+import HeroImage from "../assets/hero.jpeg";
 
 const Home : React.FC = () => {
     return (
-        <main className="flex flex-col justify-evenly items-center min-h-[100vh] w-[100%] bg-theme-black-alt">
+        <main className="flex flex-col justify-evenly items-center min-h-[100vh] w-[100%]">
+            <img src={HeroImage} className="absolute top-0 left-0 w-[100%] h-[160%] brightness-75 -z-10"/>
             <div className="flex flex-col justify-evenly md:items-start items-center md:w-[80%] w-[100%] mt-[10%]">
-                <h1 className="text-theme-white text-7xl font-bold my-4">{_EventDetails.name}</h1>
+                <h1 className="text-8xl font-extrabold my-4 text-theme">{_EventDetails.name}</h1>
                 <h2 className="text-theme-white text-5xl">{_EventDetails.type}</h2>
                 <span className="text-theme-white text-2xl tracking-wider my-2">{_EventDetails.mainquote}</span>
             </div>
