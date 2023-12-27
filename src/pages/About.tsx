92e7991bb5d93c1c5c76365159ac617b39d33db9
Roshan Thapa <thaparoshan3722@gmail.com>
2023-12-27 08:27:47 +0545

    update: small screen responsive style

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -9,9 +9,9 @@ const About : React.FC = () => {
             <h1 className="text-theme text-4xl font-bold">{"About " + _EventDetails.name + " TechFest"} </h1>
             <hr className="w-[20rem] border-2 border-theme-green rounded-xl m-8"/>
         </div>
-        <div className="flex flex-row justify-evenly items-center w-[100%]">
-            <p className="text-theme-white w-[30%] text-justify">{_EventDetails.about}</p>
-            <div className="w-[25rem] h-[25rem] m-2 bg-theme-white rounded-xl overflow-hidden"><img src={Logo} className="h-[100%]" /></div>
+        <div className="flex lg:flex-row flex-col justify-evenly items-center w-[100%]">
+            <p className="text-theme-white lg:w-[30rem] w-[80%] text-justify">{_EventDetails.about}</p>
+            <div className="xl:w-[35rem] w-[30rem] xl:h-[25rem] h-[20rem] my-8 bg-theme-white rounded-xl overflow-hidden"><img src={Logo} className="h-[100%]" /></div>
         </div>
         <div className="flex flex-col w-[100%]">
             <Stats/>
