c5860c06b72ebe3ad6ca533b6dddcc0efbe9e5c1
Roshan Thapa <thaparoshan3722@gmail.com>
2023-12-29 19:43:55 +0545

    update: landing page as home

---
+++ src/pages/Landing.tsx
@@ -0,0 +1,15 @@
+import Home from './Home.tsx';
+import About from './About.tsx';
+import Sponsor from './Sponsor.tsx';
+
+const Landing : React.FC = () => {
+  return (
+    <>
+        <Home />
+        <About />
+        <Sponsor />
+    </>
+  )
+}
+
+export default Landing
