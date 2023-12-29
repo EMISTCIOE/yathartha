7fe4efcc9b768780cd41d6f4f53f6b5affd12004
Roshan Thapa <thaparoshan3722@gmail.com>
2023-12-29 19:42:58 +0545

    add: Event moreinfo data

---
+++ src/assets/data/MoreInfo.ts
@@ -0,0 +1,92 @@
+export const _Events = [
+    {
+        title : "Grand Coding Prix",
+        date : "Magh 11",
+        duration : "24 hours",
+        thumbnail : "https://www.customerservice.ae/wp-content/uploads/2016/12/5-Tips-For-Creating-Better-Experience-At-Events.png",
+        description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet illo, explicabo voluptatum exercitationem sint aspernatur, dolore molestiae, tempora tenetur nisi maiores? Magni fugiat cupiditate similique quo aut rerum adipisci tempore atque non?"
+    },
+    {
+        title : "Break the Code",
+        date : "Magh 12",
+        duration : "4 hours",
+        thumbnail : "https://www.customerservice.ae/wp-content/uploads/2016/12/5-Tips-For-Creating-Better-Experience-At-Events.png",
+        description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet illo, explicabo voluptatum exercitationem sint aspernatur, dolore molestiae, tempora tenetur nisi maiores? Magni fugiat cupiditate similique quo aut rerum adipisci tempore atque non?"
+    },
+    {
+        title : "Gaming Fever",
+        date : "Magh 11",
+        duration : "2 hours",
+        thumbnail : "https://www.customerservice.ae/wp-content/uploads/2016/12/5-Tips-For-Creating-Better-Experience-At-Events.png",
+        description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet illo, explicabo voluptatum exercitationem sint aspernatur, dolore molestiae, tempora tenetur nisi maiores? Magni fugiat cupiditate similique quo aut rerum adipisci tempore atque non?"
+    },
+    {
+        title : "Get the Bug",
+        date : "Magh 12",
+        duration : "4 hours",
+        thumbnail : "https://www.customerservice.ae/wp-content/uploads/2016/12/5-Tips-For-Creating-Better-Experience-At-Events.png",
+        description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet illo, explicabo voluptatum exercitationem sint aspernatur, dolore molestiae, tempora tenetur nisi maiores? Magni fugiat cupiditate similique quo aut rerum adipisci tempore atque non?"
+    },
+];
+
+export const _Workshops = [
+    {
+        title : "SEO",
+        date : "Magh 11",
+        duration : "5 hours",
+        thumbnail : "https://www.kgpraigarh.ac.in/images/web_img/workshop1.png",
+        description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet illo, explicabo voluptatum exercitationem sint aspernatur, dolore molestiae, tempora tenetur nisi maiores? Magni fugiat cupiditate similique quo aut rerum adipisci tempore atque non?"
+    },
+    {
+        title : "Docker",
+        date : "Magh 11",
+        duration : "1 hours",
+        thumbnail : "https://www.kgpraigarh.ac.in/images/web_img/workshop1.png",
+        description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet illo, explicabo voluptatum exercitationem sint aspernatur, dolore molestiae, tempora tenetur nisi maiores? Magni fugiat cupiditate similique quo aut rerum adipisci tempore atque non?"
+    },
+    {
+        title : "Automation using AI/ML",
+        date : "Magh 12",
+        duration : "10 hours",
+        thumbnail : "https://www.kgpraigarh.ac.in/images/web_img/workshop1.png",
+        description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet illo, explicabo voluptatum exercitationem sint aspernatur, dolore molestiae, tempora tenetur nisi maiores? Magni fugiat cupiditate similique quo aut rerum adipisci tempore atque non?"
+    },
+    {
+        title : "Design Pattern",
+        date : "Magh 12",
+        duration : "4 hours",
+        thumbnail : "https://www.kgpraigarh.ac.in/images/web_img/workshop1.png",
+        description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet illo, explicabo voluptatum exercitationem sint aspernatur, dolore molestiae, tempora tenetur nisi maiores? Magni fugiat cupiditate similique quo aut rerum adipisci tempore atque non?"
+    }
+];
+
+export const _Competitions = [
+    {
+        title : "Idea Pitching",
+        date : "Magh 11",
+        duration : "2 hours",
+        thumbnail : "https://media.licdn.com/dms/image/C4D12AQGvunFI98D6Ig/article-inline_image-shrink_400_744/0/1645894535616?e=1707955200&v=beta&t=dO7iEMKGUgccIkOcL9DrUlqCKho8SwWXJ1kXMt5MCk4",
+        description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet illo, explicabo voluptatum exercitationem sint aspernatur, dolore molestiae, tempora tenetur nisi maiores? Magni fugiat cupiditate similique quo aut rerum adipisci tempore atque non?"
+    },
+    {
+        title : "Coding the Website",
+        date : "Magh 11",
+        duration : "8 hours",
+        thumbnail : "https://media.licdn.com/dms/image/C4D12AQGvunFI98D6Ig/article-inline_image-shrink_400_744/0/1645894535616?e=1707955200&v=beta&t=dO7iEMKGUgccIkOcL9DrUlqCKho8SwWXJ1kXMt5MCk4",
+        description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet illo, explicabo voluptatum exercitationem sint aspernatur, dolore molestiae, tempora tenetur nisi maiores? Magni fugiat cupiditate similique quo aut rerum adipisci tempore atque non?"
+    },
+    {
+        title : "Uproll the Design",
+        date : "Magh 12",
+        duration : "5 hours",
+        thumbnail : "https://media.licdn.com/dms/image/C4D12AQGvunFI98D6Ig/article-inline_image-shrink_400_744/0/1645894535616?e=1707955200&v=beta&t=dO7iEMKGUgccIkOcL9DrUlqCKho8SwWXJ1kXMt5MCk4",
+        description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet illo, explicabo voluptatum exercitationem sint aspernatur, dolore molestiae, tempora tenetur nisi maiores? Magni fugiat cupiditate similique quo aut rerum adipisci tempore atque non?"
+    },
+    {
+        title : "Innovative",
+        date : "Magh 12",
+        duration : "16 hours",
+        thumbnail : "https://media.licdn.com/dms/image/C4D12AQGvunFI98D6Ig/article-inline_image-shrink_400_744/0/1645894535616?e=1707955200&v=beta&t=dO7iEMKGUgccIkOcL9DrUlqCKho8SwWXJ1kXMt5MCk4",
+        description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet illo, explicabo voluptatum exercitationem sint aspernatur, dolore molestiae, tempora tenetur nisi maiores? Magni fugiat cupiditate similique quo aut rerum adipisci tempore atque non?"
+    }
+];
