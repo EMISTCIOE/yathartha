b2e4ea565f788df03ad26310b1c23569b1af41c5
Roshan Thapa <thaparoshan3722@gmail.com>
2024-01-04 22:15:18 +0545

    add: Pre-event and Main Event

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -1,20 +1,32 @@
-import { _Events } from "../assets/data/MoreInfo"
-import EventCard from "../components/EventCard"
+import {_PreEvents, _MainEvents } from "../assets/data/MoreInfo";
+import EventCard from "../components/EventCard";
 
 const Events : React.FC = () => {
   return (
-    <section className="min-h-[100vh] flex flex-col justify-evenly items-center w-[100%] bg-theme-black-alt pt-[8vh]">
-        <div className="flex justify-evenly flex-col items-center">
-            <h1 className="text-theme text-4xl font-bold">Events</h1>
-            <hr className="w-[8rem] border-2 border-theme-green rounded-xl my-2"/>
-        </div>
-        <div className="flex justify-around items-center flex-row flex-wrap w-[100%]">
-            {
-                _Events.map(({title, date, duration, thumbnail, description})=>
+    <section className="min-h-[100vh] flex flex-col justify-evenly items-center w-[100%] bg-theme-black-alt pt-[5rem]">
+        <div className="flex flex-col justify-evenly items-center my-8">
+            <h1 className="text-theme-red text-3xl uppercase font-bold my-4">Pre-events</h1>
+            <hr className="w-[8rem] border-2 border-theme-green rounded-xl mb-8"/>
+            <div className="flex justify-evenly items-center flex-row flex-wrap w-[100%]">
                 {
-                    return <EventCard title={title} date={date} duration={duration} thumbnail={thumbnail} description={description} />
-                })
-            }
+                    _PreEvents.map(({thumbnail, organizer, events})=>
+                    {
+                        return <EventCard thumbnail={thumbnail} organizer={organizer} events={events} />
+                    })
+                }
+            </div>
+        </div>
+        <div className="flex flex-col justify-evenly items-center my-8">
+            <h1 className="text-theme-red text-3xl uppercase font-bold my-4">Main Events</h1>
+            <hr className="w-[8rem] border-2 border-theme-green rounded-xl mb-8"/>
+            <div className="flex justify-evenly items-center flex-row flex-wrap w-[100%]">
+                {
+                    _MainEvents.map(({thumbnail, organizer, events})=>
+                    {
+                        return <EventCard thumbnail={thumbnail} organizer={organizer} events={events} />
+                    })
+                }
+            </div>
         </div>
     </section>
   )
