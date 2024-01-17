b665f2f4e94210dc0429983f39da6d498b900ed8
Roshan Thapa <thaparoshan3722@gmail.com>
2024-01-17 22:04:04 +0545

    fix: scale in small screen

--- src/components/PartnerCard.tsx
+++ src/components/PartnerCard.tsx
@@ -7,7 +7,7 @@ interface IPartnerCard {
 
 const PartnerCard : React.FC<IPartnerCard> = ({rank, name, image}) => {
   return (
-    <div className=" bg-theme-green flex flex-col justify-evenly items-center m-4 w-[25rem] max-h-[20rem] relative hover:scale-[105%] transition-transform duration-300">
+    <div className=" bg-theme-green flex flex-col justify-evenly items-center m-4 max-w-[25rem] max-h-[20rem] relative hover:scale-[105%] transition-transform duration-300">
        <div className="absolute h-[100%] w-[100%] z-10 flex flex-col justify-between items-center">
             <span className="text-2xl text-theme-red text-bold p-2 px-4 bg-theme-white rounded-bl-md rounded-br-md">{name}</span>
             <span className="text-lg text-theme-white p-2 bg-theme rounded-tl-xl rounded-tr-xl">{rank}</span>
