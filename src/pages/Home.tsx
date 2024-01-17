5bd26b9805933feab7b2730878009cdb1b3cb279
Roshan Thapa <thaparoshan3722@gmail.com>
2024-01-17 22:05:47 +0545

    update: yathartha text size in small screen

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,7 @@ const Home : React.FC = () => {
         <main className="flex flex-col justify-evenly items-center min-h-[100vh] w-[100%] relative">
             <img src={HeroImage} className="absolute top-0 left-0 min-h-[100%] xl:h-[160%] h-[160%] w-[100%] brightness-75 -z-10 object-cover"/>
             <div className="flex flex-col justify-evenly md:items-start items-center md:w-[80%] w-[100%] mt-[10%]">
-                <h1 className="xl:text-8xl sm:text-7xl text-5xl font-extrabold my-4 text-theme">{_EventDetails.name}</h1>
+                <h1 className="xl:text-8xl sm:text-7xl text-6xl font-extrabold my-4 text-theme">{_EventDetails.name}</h1>
                 <h2 className="text-theme-white lg:text-5xl text-4xl">{_EventDetails.type}</h2>
                 <span className="text-theme-white lg:text-2xl sm:text-xl text-sm tracking-wider my-2">{_EventDetails.mainquote}</span>
             </div>
